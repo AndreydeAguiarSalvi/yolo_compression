53d43b70a6c4e70747fa68344a8be0011a41d4da
AndreySalvi <andreysalvi@gmail.com>
2020-04-10 15:24:03 -0300

    .

--- test.py
+++ test.py
@@ -56,11 +56,12 @@ def test(cfg,
         verbose = False
 
     # To pruned models
-    if mask is not None:
+    if mask is not None or 'mask' in weights.keys():
         from utils.pruning import sum_of_the_weights, apply_mask_LTH, create_mask_LTH
         msk = create_mask_LTH(model)
         initial_weights = sum_of_the_weights(msk)
-        msk.load_state_dict(torch.load(mask, map_location=device))
+        if 'mask' in weights.keys(): msk.load_state_dict(torch.load(weights['mask'], map_location=device))
+        else: msk.load_state_dict(torch.load(mask, map_location=device))
         final_weights = sum_of_the_weights(msk)
         print(f'Evaluating model with initial weights number of {initial_weights} and final of {final_weights}. \nReduction of {final_weights * 100. / initial_weights}%.')
         apply_mask_LTH(model, msk)
