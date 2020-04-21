92a899f1e575cdc1425e4899ed1c7aa9f6664585
AndreySalvi <andreysalvi@gmail.com>
2020-04-21 15:06:11 -0300

    Tryng to create test files to PASCALVOC Evaluation Server

---
+++ test_pascal.py
@@ -0,0 +1,166 @@
+import os
+import argparse
+from sys import platform
+
+from models import *  # set ONNX_EXPORT in models.py
+from utils.datasets import *
+from utils.utils import *
+from utils.pruning import create_mask_LTH, apply_mask_LTH
+
+
+def detect(save_img=False):
+    img_size = (320, 192) if ONNX_EXPORT else opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
+    out, source, weights, view_img = opt.output, opt.source, opt.weights, opt.view_img
+
+    # Initialize
+    device = torch_utils.select_device(device='cpu' if ONNX_EXPORT else opt.device)
+    # Get names and colors
+    names = load_classes(opt.names)
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(len(names))]
+
+    ################
+    # Create files #
+    ################
+    if os.path.exists(out):
+        shutil.rmtree(out)  # delete output folder
+    os.makedirs(out)  # make new output folder
+    files = []
+    for f in names:
+        result_file = open(out+f'comp3_det_test_{f}.txt', 'w')
+        files.append(result_file)
+
+    # Initialize model
+    model = Darknet(opt.cfg, img_size)
+
+    # Load weights
+    attempt_download(weights)
+    if weights.endswith('.pt'):  # pytorch format
+        model.load_state_dict(torch.load(weights, map_location=device)['model'])
+    else:  # darknet format
+        load_darknet_weights(model, weights)
+
+    # Second-stage classifier
+    classify = False
+    if classify:
+        modelc = torch_utils.load_classifier(name='resnet101', n=2)  # initialize
+        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model'])  # load weights
+        modelc.to(device).eval()
+
+    # Fuse Conv2d + BatchNorm2d layers
+    # model.fuse()
+    # torch_utils.model_info(model, report='summary')  # 'full' or 'summary'
+
+    # Eval mode
+    model.to(device).eval()
+
+    # Export mode
+    if ONNX_EXPORT:
+        model.fuse()
+        img = torch.zeros((1, 3) + img_size)  # (1, 3, 320, 192)
+        f = opt.weights.replace(opt.weights.split('.')[-1], 'onnx')  # *.onnx filename
+        torch.onnx.export(model, img, f, verbose=False, opset_version=11)
+
+        # Validate exported model
+        import onnx
+        model = onnx.load(f)  # Load the ONNX model
+        onnx.checker.check_model(model)  # Check that the IR is well formed
+        print(onnx.helper.printable_graph(model.graph))  # Print a human readable representation of the graph
+        return
+
+    # Set Dataloader
+    dataset = LoadImages(source, img_size=img_size)
+
+    # Run inference
+    t0 = time.time()
+    for path, img, im0s, _ in dataset:
+        ID = path.split(os.sep)[-1]
+        t = time.time()
+
+        img = torch.from_numpy(img).to(device)
+        img = img.float()  # uint8 to fp16/32
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if img.ndimension() == 3:
+            img = img.unsqueeze(0)
+
+        # Inference
+        pred = model(img)[0]
+
+        # Apply NMS
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+
+        # Apply Classifier
+        if classify:
+            pred = apply_classifier(pred, modelc, img, im0s)
+
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+            p, s, im0 = path, '', im0s
+
+            save_path = str(Path(out) / Path(p).name)
+            s += '%gx%g ' % img.shape[2:]  # print string
+            if det is not None and len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += '%g %ss, ' % (n, names[int(c)])  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in det:
+                    print(ID, conf, *xyxy, sep=' ')
+                    # print(ID, conf, *xyxy, sep=' ', file=result_file[int(cls)])
+                    # if save_txt:  # Write to file
+                    #     with open(save_path + '.txt', 'a') as file:
+                    #         file.write(('%g ' * 6 + '\n') % (*xyxy, cls, conf))
+
+                    if save_img or view_img:  # Add bbox to image
+                        label = '%s %.2f' % (names[int(cls)], conf)
+                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)])
+                exit()
+
+            # Print time (inference + NMS)
+            print('%sDone. (%.3fs)' % (s, time.time() - t))
+
+            # Stream results
+            if view_img:
+                cv2.imshow(p, im0)
+                if cv2.waitKey(1) == ord('q'):  # q to quit
+                    raise StopIteration
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'images':
+                    cv2.imwrite(save_path, im0)
+
+    if save_img:
+        print('Results saved to %s' % os.getcwd() + os.sep + out)
+        if platform == 'darwin':  # MacOS
+            os.system('open ' + out + ' ' + save_path)
+
+    print('Done. (%.3fs)' % (time.time() - t0))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cfg', type=str, default='cfg/voc_yolov3.cfg', help='*.cfg path')
+    parser.add_argument('--names', type=str, default='data/voc.names', help='*.names path')
+    parser.add_argument('--weights', type=str, default='weights/voc_yolov3/2020_04_05/10_02_02/best.pt', help='path to weights file')
+    parser.add_argument('--mask', action='store_true', help='wheter has a mask inside the checkpoint')
+    parser.add_argument('--mask_weight', type=str, default=None, help='wheter mask is another checkpoint')
+    parser.add_argument('--source', type=str, default='./PASCALVOC2012/images/test/', help='source')  # input file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default=None, required=True, help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.3, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.6, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='device id (i.e. 0 or 0,1) or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    opt = parser.parse_args()
+    print(opt)
+
+    with torch.no_grad():
+        detect()
