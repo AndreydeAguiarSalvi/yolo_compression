cd76df43d7d35bc5b0422c937e589350d740ba60
AndreySalvi <andreysalvi@gmail.com>
2020-06-03 19:56:03 -0300

    updating sparse model creation

--- graphs.py
+++ graphs.py
@@ -5,9 +5,9 @@ from torch.utils.tensorboard import SummaryWriter
 
 x = torch.Tensor(1, 3, 416, 416)
 
-model = YOLO_Nano()
+model = Darknet(cfg='cfg/voc_yolov3.cfg')
 y = model(x)
 
-tb = SummaryWriter('debugs/yolo_nano')
+tb = SummaryWriter('debugs/yolov3')
 tb.add_graph(model, x)
 tb.close()
