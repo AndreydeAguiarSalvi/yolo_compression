bb8639b9ac7eef23bd70df3fb842099bc991c84a
AndreydeAguiarSalvi <andreysalvi@gmail.com>
2020-12-11 15:59:43 -0300

    Adding temporal script to visualize fastly the generated features

---
+++ compare_fts.py
@@ -0,0 +1,77 @@
+import os
+import cv2
+import argparse
+import numpy as np
+
+
+def show_images(original_pth: list, teacher_pth: list, reduced_pth: list, student_pth: list):
+    for i, (tch, red, std) in enumerate(zip(teacher_pth, reduced_pth, student_pth)):
+        img1 = cv2.imread(original_pth[i % 9])
+        img2 = cv2.imread(tch)
+        img3 = cv2.imread(red)
+        img4 = cv2.imread(std)
+        stacked = np.concatenate((img1, img2, img3, img4), axis=1)
+        window_name = tch.split(os.sep)[-1]
+        print(stacked.shape, window_name)
+        
+        # cv2.namedWindow("Window", cv2.WINDOW_KEEPRATIO)
+        # cv2.imshow("Window", stacked)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+        # cv2.namedWindow("Window", cv2.WINDOW_KEEPRATIO)
+        # cv2.imshow("Window", stacked)
+        # while cv2.getWindowProperty(window_name, cv2.WND_PROP_VISIBLE) >= 1:
+        #     keyCode = cv2.waitKey(10000)
+        #     if (keyCode & 0xFF) == ord('q'):
+        #         cv2.destroyAllWindows()
+        #         break
+        cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+        cv2.moveWindow(window_name, 0, 0)
+        cv2.imshow(window_name, stacked)
+        key = cv2.waitKey(10000) #pauses for 10 seconds before fetching next image
+        if key == 27:#if ESC is pressed, exit loop
+            cv2.destroyAllWindows()
+            break
+
+
+def load_paths(r: str, model: str, visu: str, loss: str) -> list:
+    result = []
+    for root, _, files in os.walk(r):
+        for f in files:
+            if model in root and visu in f and loss in root:
+                result.append(root + os.sep + f)
+    result.sort()
+    return result
+
+
+def get_original_imgs(paths: list) -> list:
+    result = []
+    for f in paths:
+        parts = f.split(os.sep)
+        last, ext = parts[-1].split('.')[0], parts[-1].split('.')[1]
+        year, id = last.split('_')[2], last.split('_')[3]
+        original = ''
+        for i in range(len(parts)-1): original += parts[i] + os.sep
+        original += year + '_' + id + '.' + ext
+        result.append(original)
+    return result
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--teacher', default='v3', help='folder model used as teacher')
+    parser.add_argument('--reduced', default='nano', help='folder model used as student, but in normal training')
+    parser.add_argument('--student', default='nano_kd36', help='folder model used as student, trained with KD')
+    parser.add_argument('--root', default='output/GradCam', help='root folder with features to visualize')
+    parser.add_argument('--visu', default='grad', choices=['grad', 'cam-gb', 'gb'], help='kind of visualization')
+    parser.add_argument('--loss', default='self', help='kind of loss used to compute the visualizations. If self, means the pseudo-original GradCam loss,\
+        otherwise, it expects a number')
+    args = vars(parser.parse_args())
+
+    teacher_pth = load_paths(args['root'], args['teacher'], args['visu'], args['loss'])
+    reduced_pth = load_paths(args['root'], args['reduced'] + os.sep, args['visu'], args['loss'])
+    student_pth = load_paths(args['root'], args['student'], args['visu'], args['loss'])
+    original_pth = get_original_imgs(teacher_pth)
+    
+    show_images(original_pth, teacher_pth, reduced_pth, student_pth)
