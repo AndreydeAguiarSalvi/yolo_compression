2bbbfbe5e05e9506abd99482559f4fcc1370e7c5
AndreydeAguiarSalvi <andreysalvi@gmail.com>
2021-01-26 14:49:29 -0300

    Fixing bug: counting parameters and MACs when model is original SoftMaskedConv2d

--- computing_flops.py
+++ computing_flops.py
@@ -1,15 +1,22 @@
 import torch
 import argparse
-from thop import profile
 from models import *
+from thop import profile
+from utils.layers import M2MSparseConv, SoftMaskedConv2d
 from utils.pruning import create_mask_LTH, apply_mask_LTH
 
 
 def count_M2MSparsable(m, x, y):
     m.total_ops = torch.DoubleTensor([m.parameters().numel() * m.IN_SH[-1]])
 
+def count_SoftConv(m, x, y):
+    kernel_ops = torch.zeros(m.weight.size()[2:]).numel()
+    total_ops = y.nelement() * (m.in_channels * kernel_ops)
+    m.total_ops += torch.DoubleTensor([int(total_ops)])
+
 custom_hooks = {
-    M2MSparseConv: count_M2MSparsable
+    M2MSparseConv: count_M2MSparsable,
+    SoftMaskedConv2d: count_SoftConv
 }
 
 
@@ -19,30 +26,23 @@ parser.add_argument('--cfg', type=str, help='args file to create the model.')
 parser.add_argument('--mask', type=str, default=None, help='Path to load the mask, if existis.')
 parser.add_argument('--embbed', action='store_true', help='To load the mask from the same checkpoint of model.')
 parser.add_argument('--device', help='cuda:id or cpu', required=True)
-# parser.add_argument('--clever_format', action='store_true')
+parser.add_argument('--macs_reduction', default='full_sparse', choices=['full_sparse', 'channel_sparse', 'none'], 
+    help='Which approach for MACs reduction. none means use the default model')
 args = vars(parser.parse_args())
 
 device = torch.device(args['device'])
 x = torch.Tensor(1, 3, 416, 416).to(device)
 
 # Initialize model
-if 'soft' in args['model'] or 'soft' in args['cfg']:
-    model = SoftDarknet(args['cfg']).to(device)
-    model.ticket = True
-    _ = model(x)
-else:
-    if 'nano' in args['cfg']: model = YOLO_Nano(args['cfg']).to(device)
-    else: model = Darknet(args['cfg']).to(device)    
+if 'soft' in args['cfg']: model = SoftDarknet(args['cfg']).to(device)
+elif 'nano' in args['cfg']: model = YOLO_Nano(args['cfg']).to(device)
+else: model = Darknet(args['cfg']).to(device)
 
 if args['model']:
     checkpoint = torch.load(args['model'], map_location=device)
     try:
-        try:
-            model.load_state_dict(checkpoint['model'])
-        except:        
-            model.load_state_dict(checkpoint) 
+        model.load_state_dict(checkpoint['model'])
     except:
-        print("model key don't found in checkpoint. Trying without model key")
         model.load_state_dict(checkpoint)
 
 if (args['mask'] or args['embbed']):
@@ -51,11 +51,25 @@ if (args['mask'] or args['embbed']):
     else: mask.load_state_dict(checkpoint['mask'])
     apply_mask_LTH(model, mask)
 
-if not (args['mask'] or args['embbed'] or 'soft' in args['model']): total_ops, total_params = profile(model, (x,), verbose=True)
-else:
+elif 'soft' in args['cfg']:
+    model.ticket = True
+    model.temp = 1.
+    _ = model(x)
+
+if not (args['mask'] or args['embbed'] or 'soft' in args['model']) or args['macs_reduction'] == 'none': 
+    print('using model with no MACs reduction')
+    total_ops, total_params = profile(model, (x,), custom_ops=custom_hooks, verbose=True)
+
+elif args['macs_reduction'] == 'full_sparse':
+    print('using FullSparseYOLO for MACs reduction')
+    sparse = FullSparseYOLO(model).to(device)
+    total_ops, total_params = profile(sparse, (x, ), custom_ops=custom_hooks, verbose=True)
+
+elif args['macs_reduction'] == 'channel_sparse':
+    print('using Ch_Wise_SparseYOLO for MACs reduction')
     sparse = Ch_Wise_SparseYOLO(model).to(device)
     total_ops, total_params = profile(sparse, (x, ), custom_ops=custom_hooks, verbose=True)
 
 print("%s | %s" % ("Params", "MACs"))
 print("---|---")
-print("%.2f | %.2f" % (total_params, total_ops ))
+print(f"{total_params:,}\t{total_ops:,}")
